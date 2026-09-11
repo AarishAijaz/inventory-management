fc2bbc7532fb89bf0119c349b36dfdf37c137ffc
Aarish Aijaz <aarishaijaz9@gmail.com>
2026-09-11 13:03:38 +0500

    Initial Commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider, Show, SignIn, UserButton } from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,11 +20,22 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
-    </html>
+    <ClerkProvider>
+      <html
+        lang="en"
+        className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      >
+        <body className="min-h-full flex flex-col">
+          <header>
+            <h1>Dashboard</h1>
+            <UserButton showName />
+          </header>
+          <main>
+            <Show when="signed-out"><SignIn routing="hash" /></Show>
+            <Show when="signed-in">{children}</Show>
+          </main>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
